981049c9a2a97b3af7b4fd27d098956560ee7875
Tue NGO <tue.ngodinh@papernest.com>
2024-03-22 18:09:40 +0100

    POC WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CSP_NONCE, Inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DOCUMENT } from '@angular/common';
+import { nonceFactory } from './nonce.factory';
+
+const nonce = nonceFactory()
 
 @NgModule({
   declarations: [
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    // { provide: CSP_NONCE, useValue: nonce }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(@Inject(DOCUMENT) document: Document) {
+    document.querySelector('meta[http-equiv="Content-Security-Policy"]')?.setAttribute('content', `default-src 'self'; img-src 'self' data:; script-src 'self' 'nonce-${ nonce }'; style-src 'self' 'nonce-${ nonce }`);
+    document.querySelector('app-root')?.setAttribute('ngCspNonce', nonce);
+  }
+}
